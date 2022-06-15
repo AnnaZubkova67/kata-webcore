0a34894a0130260f52230a58403b1502d0e97b49
Anna Zubkova <annazubkowa12@gmail.com>
2022-06-15 14:34:52 +0400

    first new

--- functional.js
+++ functional.js
@@ -1,43 +1,29 @@
 const slider = document.querySelector('.swiper');
-let mySwiper;
-function mobileSlider() {
-    if (window.innerWidth < 768 && slider.dataset.mobile == "false") {
-        document.querySelector('.swiper-pagination').style.display = "block",
-            mySwiper = new Swiper(slider, {
-                slideClass: "brands",
-                slidesPerView: 1.3,
-                pagination: {
-                    el: '.swiper-pagination',
-                    clickable: true,
-                },
-            });
-        slider.dataset.mobile = "true";
-    }
-    if (window.innerWidth >= 768) {
-        slider.dataset.mobile = "false";
-        if (slider.classList.contains('swiper-initialized')) {
-            mySwiper.destroy();
-            document.querySelector('.swiper-pagination').style.display = "none";
-        }
-    }
-}
-window.addEventListener('resize', function () {
-    mobileSlider();
+let mySwiper = new Swiper(slider, {
+    slideClass: "brands",
+    slidesPerView: 1.3,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
 });
-mobileSlider();
-    let linkOnwards = document.querySelector('.link-onwards_type_show');
-    let content = document.querySelector('.navigation__cards')
-    linkOnwards.addEventListener('click', function () {
-        if (content.classList.contains("open")) {
-            content.classList.remove("open");
-            linkOnwards.textContent = "Показать все";
-            linkOnwards.classList.remove('hide');
-        } else {
-            content.classList.toggle('open');
+if (window.innerWidth >= 768) {
+    mySwiper.destroy();
+    document.querySelector('.swiper-pagination').style.display = "none";
+}
+let linkOnwards = document.querySelector('.link-onwards_type_show');
+let content = document.querySelector('.navigation__cards')
+linkOnwards.addEventListener('click', function () {
+    if (content.classList.contains("open")) {
+        content.classList.remove("open");
+        linkOnwards.textContent = "Показать все";
+        linkOnwards.classList.remove('hide');
+    } else {
+        content.classList.toggle('open');
         linkOnwards.classList.add('hide');
-            linkOnwards.textContent = "Скрыть";
-        }
-    });
+        linkOnwards.textContent = "Скрыть";
+    }
+});
 
 
 
