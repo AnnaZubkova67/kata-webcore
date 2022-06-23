405dd3fcbc1bfb539cebdd422bde6d56b81245ec
Anna Zubkova <annazubkowa12@gmail.com>
2022-06-23 20:30:45 +0400

    first new

--- block7/src/js/closed_modal.js
+++ block7/src/js/closed_modal.js
@@ -2,11 +2,13 @@ export let closed = document.querySelectorAll('.button-closed');
 export let modalCall = document.querySelector('.modal--type--call');
 export let modalFeedback = document.querySelector('.modal--type--feedback');
 export let modalMenu = document.querySelector('.modal--type--menu');
+export let bodyContent = document.querySelector('.body')
 export function closedModal(classModal) {
     for (let i = 0; i < closed.length; i++) {
         closed[i].addEventListener('click', function (evt) {
             evt.preventDefault();
             classModal.classList.remove('open-modal');
+          bodyContent.style.overflow = 'auto';
         });
     }
 }
--- block7/src/js/open_modal.js
+++ block7/src/js/open_modal.js
@@ -4,14 +4,16 @@ export let modalFeedback = document.querySelector('.modal--type--feedback');
 export let modalMenu = document.querySelector('.modal--type--menu');
 export let buttonChat = document.querySelectorAll('.body__button-chat');
 export let buttonMenu = document.querySelectorAll('.body__button-menu');
+export let bodyContent = document.querySelector('.body')
 export function openModal(classModal, buttonModal) {
     for (let i = 0; i < buttonModal.length; i++) {
         buttonModal[i].addEventListener('click', function (evt) {
             if (modalMenu.classList.contains('open-modal')) {
-                modalMenu.classList.remove('open-modal')
+                modalMenu.classList.remove('open-modal');
             }
             evt.preventDefault();
             classModal.classList.add('open-modal');
+          bodyContent.style.overflow = 'hidden';
         });
     }
 }
