6d1c4f7a57b46a114f523b9e2e33367fcaec4cdd
Anna Zubkova <annazubkowa12@gmail.com>
2022-06-28 12:40:12 +0400

    first new

--- block7/src/js/index.js
+++
@@ -1,6 +0,0 @@
-import '../scss/style.scss';
-import {buttonCall , modalCall, modalFeedback, modalMenu, buttonChat, buttonMenu , bodyContent, openModal} from "../js/open_modal";
-import {linkOnwards,content, openContent} from "../js/open_content";
-import {modal, modalContent,closed, closedModal} from "../js/closed_modal";
-import {slider, sliderBrands, sliderTypes, sliderServices, newSwiper, swiper} from "../js/swiper";
-
---
+++ src/js/index.js
@@ -0,0 +1,6 @@
+import '../scss/style.scss';
+import {buttonCall , modalCall, modalFeedback, modalMenu, buttonChat, buttonMenu , bodyContent, openModal} from "./open_modal";
+import {linkOnwards,content, openContent} from "./open_content";
+import {modal, modalContent,closed, closedModal} from "./closed_modal";
+import {slider, sliderBrands, sliderTypes, sliderServices, newSwiper, swiper} from "./swiper";
+
